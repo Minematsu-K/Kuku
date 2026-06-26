a8a1620c6d636cbe51ef767cd580ee887c4c451d
Minematsu <bsi26045@uhe.ac.jp>
2026-06-26 15:32:00 +0900

    テキストを追加

--- src/Kuku.java
+++ src/Kuku.java
@@ -3,6 +3,7 @@ public class Kuku {
 
 	public static void main(String[] args) {
 		
+		System.out.println("九九の表");
 		for(int i=1; i<=9; i++) {
 			for(int j=1; j<=9; j++) {
 				System.out.println(i+"×"+j+"="+i*j);
